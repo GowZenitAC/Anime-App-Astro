985b0214e34ece10cdda7422b47e721f0191d626
GowZenitAC <jes_alpuche@outlook.es>
2023-12-07 20:12:19 -0600

    Consumo de api y creacion de vista principañ

--- src/data/animes.js
+++ src/data/animes.js
@@ -1,28 +1,6 @@
-export const animes = [
-    {
-        "id":1,
-        "nombre":"Jujutsu Kaisen",
-        "temporadas": "1",
-        "sinopsis":"Yuuji Itadori es un estudiante de instituto con unas habilidades físicas excepcionales. Todos los días, como rutina, va al hospital a visitar a su abuelo enfermo y decide apuntarse al club de ocultismo del instituto para no dar un palo al agua… Sin embargo, un buen día el sello del talismán que se hallaba escondido en su instituto se rompe, y comienzan a aparecer unos monstruos. Ante este giro de los acontecimientos, Itadori decide adentrarse en el instituto para salvar a sus compañeros. ¿Qué le deparará el destino?"
-
-    },
-    {
-        "id":2,
-        "nombre":"Code Geas",
-        "temporadas": "2",
-        "sinopsis":"Nos situamos en una realidad alternativa, donde el Imperio de Britania ha conquistado prácticamente todo el planeta. A los países subyugados bajo su poder opresor les ha arrebatado la libertad, los derechos e incluso el nombre. Área 11, ésa es la denominación que ahora tienen los japoneses. Allí, un joven llamado Lelouch comenzará su rebelión, el día en que por casualidad se topa con la misteriosa C.C., quien le concede un poder peculiar: El Geass, el poder de la absoluta obediencia. Basta con cruzar su mirada con la de su objetivo para que éste cumpla cualquier orden que Lelouch realice, sea cual sea, aunque signifique su propia destrucción."
-    },
-    {
-        "id":3,
-        "nombre":"Slam Dunk",
-        "temporadas": "1",
-        "sinopsis":"Un estudiante de preparatoria llamado Hanamichi Sakuragi, que sufre una serie de rechazos amorosos hasta que un buen día conoce a una chica a quien le fascina el baloncesto. A causa de esto Hanamichi fingirá ser un deportista y se inscribirá en el club de baloncesto del colegio con la esperanza de poder impresionarla y salir con ella. Aunque con el tiempo descubrirá en el baloncesto un deporte que lo exigirá y pondrá a prueba sacando lo mejor de él."
-    },
-    {
-        "id":4,
-        "nombre":"Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai",
-        "temporadas": "1",
-        "sinopsis":"Síndrome de la pubertad: ciertas experiencias poco corrientes que se rumorea en Internet que son la causa del exceso de sensibilidad e inestabilidad durante la adolescencia. Este año, Sakuta Azusagawa, estudiante de segundo en una preparatoria cercana a Enoshima, conocerá a varias chicas que están pasando por ese síndrome de la pubertad. Por ejemplo, en la biblioteca conoce a una salvaje chica conejito que resulta ser Mai Sakurajima, una chica mayor de su preparatoria que es una actriz que está tomándose un descanso. ¿El problema? Que nadie parece poder ver a esa chica tan encantadora, ¿cómo es posible? Mientras Sakuta busca respuestas para ayudar a Mai comienzan a forjar un vínculo y terminará descubriendo los sentimientos que la chica oculta en su corazón... Una historia que se sale de lo habitual y que se desarrolla en una ciudad con un luminoso cielo y un brillante océano donde Sakuta conocerá a varias chicas de lo más misteriosas e intrigantes."
-    }
-
-]
+const url = "https://api.jikan.moe/v4/anime";
+export const data = async () =>{
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.data;
+}
