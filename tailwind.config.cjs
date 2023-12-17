8a4c53f00aa4fd58dd7f3dbbee93984165f53a7a
GowZenitAC <jes_alpuche@outlook.es>
2023-12-16 21:00:38 -0600

    se añade diseño responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				'2xs': '0.55rem',
+			}
+		},
 	},
 	plugins: [],
 }
